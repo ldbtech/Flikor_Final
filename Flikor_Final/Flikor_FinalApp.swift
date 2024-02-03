0ce0a58c4ca80e4559f83b92de61d7b794d0a088
Mohamed Ali Larbi Daho Bachir <alildb2036@gmail.com>
2024-02-03 05:33:17 -0500

    Initial Commit

---
+++ Flikor_Final/Flikor_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flikor_FinalApp.swift
+//  Flikor_Final
+//
+//  Created by Ali Daho on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Flikor_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
